f3d4fdf87dd6bba7c67f930ed7afd5745249d7c2
Iury Miguel <iurymiguel@MacBook-Pro-do-Iury-Miguel.local>
2019-01-30 14:16:31 -0200

    Initial Commit

---
+++ MoviesApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MoviesApp
+//
+//  Created by Iury Miguel on 30/01/19.
+//  Copyright © 2019 Iury Miguel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
